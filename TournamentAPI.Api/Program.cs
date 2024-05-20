e673c21afbb9e7784a6271fb6bf65323425807fa
Tina Bruce <tina@fishface.se>
2024-05-20 20:48:44 +0200

    Add project files.

---
+++ TournamentAPI.Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TournamentAPI.Data.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TournamentAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TournamentAPIContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
